be6714f84cbe596fea8c98d036bfeb4dac1b0e89
Marcin Polonis <marcin.polonis@o2.pl>
2019-03-27 21:12:34 +0100

    Correct structure in todolist.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,16 +2,16 @@ import React from 'react';
 import style from '../containers/List.css';
 
 const TodoList = props => {
-const listToDo = props.data.map((item) => <li key={item.id} onClick={() => props.remove(item.id)}>{item.text}</li>);
+    const listToDo = props.data.map((item) => <li key={item.id} onClick={() => props.remove(item.id)}>{item.text}</li>);
 
-return (
-    <div>
-        <h2>Tasks to do:</h2>
-        <ul className={style.ToDoList}>
-            {listToDo}
-        </ul>
-    </div>
-)
+    return (
+        <div>
+            <h2>Tasks to do:</h2>
+            <ul className={style.ToDoList}>
+                {listToDo}
+            </ul>
+        </div>
+    )
 }
 
 export default TodoList;
